ec15a5dfbba477bc18eb3c236790a9363a86476f
Théo <theo.cvnt@gmail.com>
2022-09-19 17:24:16 +0200

    code

--- papaoutai.py
+++ papaoutai.py
@@ -15,22 +15,25 @@ parser.add_argument("--vcf", required=True, help= "Vcf should contain the id\
 parser.add_argument("--m", required=True, help="Maternal id")
 parser.add_argument("--c1", required=True, help="Child1 id")
 parser.add_argument("--c2", required=True, help="Child2 id")
-parser.add_argument("--f", required=False, help="Father id for validation")
+parser.add_argument("--threshold", required=False, help="Probability threshold to\
+    use a reconstructed heterzoygous site", default=0.9)
 args = parser.parse_args()
 
 #-----------------------------------------------------------------------------#
-# 1. Prepare two table
-# - path_table -> hold the probabilities of each possible paternal genotypes
+# 1. Prepare table holding the probabilities of each possible paternal genotypes
 # for each combination of mother, child1 and child2 genotypes
-# - gp_table -> hold the probability of the genotype of the mother, child1
-# and child2 given by glimpse
 #-----------------------------------------------------------------------------#
 
 path_table_gp_way = numpy.array([
             [4/5, 1/5, 0],
             [0, 1, 0],
+            [0, 0, 0],
             [0, 1, 0],
             [0, 1/5, 4/5],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
 
             [4/5, 1/5, 0],
             [1/2, 1/2, 0],
@@ -42,87 +45,85 @@ path_table_gp_way = numpy.array([
             [0, 1/2, 1/2],
             [0, 1/5, 4/5],
 
-            [0, 1/5, 4/5],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+
+            [0, 0, 0],
+            [4/5, 1/5, 0],
             [0, 1, 0],
+
+            [0, 0, 0],
             [0, 1, 0],
-            [4/5, 1/5, 0]
+            [0, 1/5, 4/5]
 ],
 dtype=float)
 
+m_path = [i//9 for i in range(27)]
+c1_path = [(i//3)%3 for i in range(27)]
+c2_path = [i%3 for i in range(27)]
 
 #-----------------------------------------------------------------------------#
 # 2. Open the vcf containg mother, child1 and child2
+# and reconstruct the sites
 #-----------------------------------------------------------------------------#
-
-class GPvector:
-    def __init__(self, p_GPm, p_GPc1, p_GPc2):
-        self.vector = numpy.array([0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0], dtype=float)
-        self.GPm, self.GPc1, self.GPc2 = p_GPm, p_GPc1, p_GPc2
-        self.go_first_branch, self.go_third_branch = True, True
-
-        # populate vector
-        self.maternal_branches_validation()
-        self.maternal_branches_modification()
-        self.multiply_GPs()
-    
-    def maternal_branches_validation(self):
-        # first branch
-        if self.GPc1[:2] == (0, 0) or self.GPc2[:2] == (0, 0):
-            self.go_first_branch = False
-
-        # last branch
-        if self.GPc1[1:] == (0, 0) or self.GPc2[1:] == (0, 0):
-            self.go_third_branch = False
-    
-    def maternal_branches_modification(self):
-        if not self.go_first_branch and not self.go_third_branch:
-            self.GPm = (0, 1, 0)
-        elif not self.go_first_branch:
-            self.GPm = (0, self.GPm[1]/sum(self.GPm[1:]), self.GPm[2]/sum(self.GPm[1:]))
-        elif not self.go_third_branch:
-            self.GPm = (self.GPm[0]/sum(self.GPm[:2]), self.GPm[1]/sum(self.GPm[:2]), 0)
-    
-    def multiply_GPs(self):
-        v=0
-        # first branch
-        if self.go_first_branch:
-            for c1, c2 in zip((0,0,1,1), (0,1,0,1)):
-                self.vector[v] = self.GPm[0]*(self.GPc1[c1]/sum(self.GPc1[:2]))*(self.GPc2[c2]/sum(self.GPc2[:2]))
-                v+=1
-        else:
-            v+=4
-        # second branch
-        for c1, c2 in zip((0,0,0,1,1,1,2,2,2), (0,1,2,0,1,2,0,1,2)):
-            self.vector[v] = self.GPm[1]*self.GPc1[c1]*self.GPc2[c2]
-            v+=1
-        # third branch
-        if self.go_third_branch:
-            for c1, c2 in zip((1,1,2,2), (1,2,1,2)):
-                self.vector[v] = self.GPm[2]*(self.GPc1[c1]/sum(self.GPc1[1:]))*(self.GPc2[c2]/sum(self.GPc2[1:]))
-                v+=1
-    
-    def __str__(self):
-        return str(self.vector)
 
 M, C1, C2 = args.m, args.c1, args.c2
 vcf_reader = VariantFile(args.vcf, 'r')
 vcf_reader.subset_samples([M, C1, C2])
 
+imputation = []
+impute_none = 0
 for rec in vcf_reader:
 
     GPm, GPc1, GPc2 = rec.samples[M]['GP'], rec.samples[C1]['GP'], rec.samples[C2]['GP']
 
-    # do not take in account impossible cases
+    # do not take in account impossible cases AND only consider biallelic sites
     if not (((GPc1[:2] == (0, 0) or GPc2[:2] == (0, 0)) and GPm[1:] == (0, 0)) or \
-        (GPc1[1:] == (0, 0) or GPc2[1:] == (0, 0) and GPm[:2] == (0,0))):
+        (GPc1[1:] == (0, 0) or GPc2[1:] == (0, 0) and GPm[:2] == (0,0))) \
+            and len(rec.alts)==1:
 
-        gp_vec = GPvector(GPm, GPc1, GPc2)
-        GPf_0, GPf_1, GPf_2= numpy.dot(gp_vec.vector, path_table_gp_way)
-        print(GPf_0, GPf_1, GPf_2)
+        gp_vec = [GPm[m]*GPc1[c1]*GPc2[c2] for m, c1, c2 in zip(m_path, c1_path, c2_path)]
+        GPf_0, GPf_1, GPf_2= numpy.dot(gp_vec, path_table_gp_way)
+        normGPf_0, normGPf_1, normGPf_2 = GPf_0/(GPf_0+GPf_1+GPf_2), GPf_1/(GPf_0+GPf_1+GPf_2), GPf_2/(GPf_0+GPf_1+GPf_2)
+        genos_prob = [normGPf_0, normGPf_1, normGPf_2 ]
 
-        if sum([GPf_0, GPf_1, GPf_2]) > 1.01 or sum([GPf_0, GPf_1, GPf_2]) < 0.99:
-            raise Exception("No 0", sum([GPf_0, GPf_1, GPf_2]))
+        if GPf_0 != GPf_1 != GPf_2 != 1/3:
+            imputation.append(genos_prob.index(max(genos_prob)))
+        else:
+            imputation.append(None)
+            impute_none+=1
+    else:
+        imputation.append(None)
+        impute_none+=1
+
+vcf_reader.close()
+
+#-----------------------------------------------------------------------------#
+# 3. Write the imputed genotypes of the father
+#-----------------------------------------------------------------------------#
+
+geno_to_alleles = {
+    0: (0,0),
+    1: (0,1),
+    2: (1,1),
+    None: (None, None)
+}
+
+# write header with father
+vcf_reader = VariantFile(args.vcf, 'r')
+vcf_reader.subset_samples([args.c1]) # I change this in dividual to FATHER at each SNPs
+
+vcf_header = vcf_reader.header.__str__().split('\n')
+print('\n'.join(vcf_header[:-2]))
+print('\t'.join(['#CHROM', "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", "FATHER"]))
+
+writing_None = 0
+itr=0
+for rec, imput in zip(vcf_reader, imputation):
+
+    # put paternal GT
+    rec.samples[args.c1]['GT'] = geno_to_alleles[imput]
+    print(str(rec)[:-1])
 
 vcf_reader.close()
